5897215b61f5485873c72ec8f4e4459b40c2d5d0
Henrik Lied <henrik.lied@nrk.no>
2024-01-15 09:28:34 +0100

    Trying to workaround the start offset by int'ing it

--- whisper_live/client.py
+++ whisper_live/client.py
@@ -189,7 +189,7 @@ class Client:
                     continue
                 seg['channel'] = self.channel
                 seg['session'] = self.session
-                seg['start'] = int(seg['start'])
+                seg['start'] = int(float(seg['start']))
                 redis_client.publish(channel_name, json.dumps(seg))
                 text.append(seg["text"])
 
